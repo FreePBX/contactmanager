8a239038e1266d22792fd7f8b8262f8cde758ac3
Andrew Nagy <andrew.nagy@the159.com>
2015-04-08 21:32:24 -0400

    look and feel in usermanager

--- views/userman_hook.php
+++ views/userman_hook.php
@@ -1,27 +1,25 @@
-<style>
-.indent-div {
-	margin-left: 15px;
-}
-</style>
-<table>
-	<tr class="guielToggle" data-toggle_class="contactmanager">
-		<td colspan="2" ><h4><span class="guielToggleBut">-  </span><?php echo _("Contact Manager")?></h4><hr></td>
-	</tr>
-	<tr>
-		<td colspan="2">
-			<div class="indent-div">
-				<table>
-					<tr class="contactmanager">
-						<td><?php echo _('Show contact in Contact Manager')?></td>
-						<td>
-							<span class="radioset">
-								<input type="radio" id="contactmanager1" name="contactmanager_show" value="true" <?php echo ($enabled) ? 'checked' : ''?>><label for="contactmanager1"><?php echo _('Yes')?></label>
-								<input type="radio" id="contactmanager2" name="contactmanager_show" value="false" <?php echo (!$enabled) ? 'checked' : ''?>><label for="contactmanager2"><?php echo _('No')?></label>
-							</span>
-						</td>
-					</tr>
-				</table>
+<div class="element-container">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="row">
+				<div class="form-group">
+					<div class="col-md-3">
+						<label class="control-label" for="contactmanager_show"><?php echo _('Contact Manager')?></label>
+						<i class="fa fa-question-circle fpbx-help-icon" data-for="contactmanager_show"></i>
+					</div>
+					<div class="col-md-9">
+						<span class="radioset">
+							<input type="radio" id="contactmanager1" name="contactmanager_show" value="true" <?php echo ($enabled) ? 'checked' : ''?>><label for="contactmanager1"><?php echo _('Yes')?></label>
+							<input type="radio" id="contactmanager2" name="contactmanager_show" value="false" <?php echo (!$enabled) ? 'checked' : ''?>><label for="contactmanager2"><?php echo _('No')?></label>
+						</span>
+					</div>
+				</div>
 			</div>
-		</td>
-	</tr>
-</table>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<span id="contactmanager_show-help" class="help-block fpbx-help-block"><?php echo _("Whether to show this contact in contact manager.")?></span>
+		</div>
+	</div>
+</div>
