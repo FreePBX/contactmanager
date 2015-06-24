55f83f7d3a7dc29c205a86611f490e2949adf0ca
Andrew Nagy <andrew.nagy@the159.com>
2015-06-23 20:12:44 -0400

    add wrapper for scrolling inside

--- views/grid.php
+++ views/grid.php
@@ -8,11 +8,17 @@
 		<?php foreach($types as $type => $data) {?>
 			<div id="<?php echo $type?>" class="tab-pane <?php echo $type == "internal" ? "active" : ""?>">
 				<?php if(!empty($groups[$type])) { ?>
-					<ul class="nav nav-tabs" role="tablist">
-					<?php foreach($groups[$type] as $k => $group) { ?>
-						<li data-name="<?php echo $type?>-<?php echo $group['id']?>" class="change-tab <?php echo $k== 0 ? "active" : ""?>"><a href="#<?php echo $type?>-<?php echo $group['id']?>" aria-controls="<?php echo $type?>-<?php echo $group['id']?>" role="tab" data-toggle="tab"><?php echo $group['name']?></a></li>
-					<?php } ?>
-					</ul>
+					<div class="nav-container">
+						<div class="scroller scroller-left"><i class="glyphicon glyphicon-chevron-left"></i></div>
+						<div class="scroller scroller-right"><i class="glyphicon glyphicon-chevron-right"></i></div>
+						<div class="wrapper">
+							<ul class="nav nav-tabs list" role="tablist">
+							<?php foreach($groups[$type] as $k => $group) { ?>
+								<li data-name="<?php echo $type?>-<?php echo $group['id']?>" class="change-tab <?php echo $k== 0 ? "active" : ""?>"><a href="#<?php echo $type?>-<?php echo $group['id']?>" aria-controls="<?php echo $type?>-<?php echo $group['id']?>" role="tab" data-toggle="tab"><?php echo $group['name']?></a></li>
+							<?php } ?>
+							</ul>
+						</div>
+					</div>
 					<div class="tab-content display">
 						<?php foreach($groups[$type] as $k => $group) {?>
 							<div id="<?php echo $type?>-<?php echo $group['id']?>" class="tab-pane <?php echo $k== 0 ? "active" : ""?>">
