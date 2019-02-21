b82150efc339c4639d862b78a03f584a841c594b
Andrew Nagy <andrew.nagy@the159.com>
2019-02-20 20:39:36 -0500

    [Module Tag script: contactmanager 15.0.8.4] fixing legacy restore

--- Restore.php
+++ Restore.php
@@ -8,22 +8,7 @@ class Restore Extends Base\RestoreBase{
 	}
 
 	public function processLegacy($pdo, $data, $tables, $unknownTables){
-		$tables = [
-			'contactmanager_groups',
-			'contactmanager_groups_entries',
-			'contactmanager_entry_speeddials',
-			'contactmanager_entry_numbers',
-			'contactmanager_entry_images',
-			'contactmanager_entry_userman_images',
-			'contactmanager_entry_xmpps',
-			'contactmanager_entry_emails',
-			'contactmanager_entry_websites'
-		];
-		foreach($tables as $table) {
-			$sth = $pdo->query("SELECT * FROM $table",\PDO::FETCH_ASSOC);
-			$res = $sth->fetchAll();
-			$this->addDataToTableFromArray($table, $res);
-		}
+		$this->restoreLegacyDatabaseKvstore($pdo);
 	}
 
 }
