428f360f137787d734acc3b7d09324831496e585
Prashob K Thamban <prashobk@sangoma.com>
2022-02-25 15:07:28 +0530

    FREEPBX-23229 fixed the issue of empty contact entries getting created in the contacts when we bulk import contacts

--- Contactmanager.class.php
+++ Contactmanager.class.php
@@ -3386,14 +3386,16 @@ class Contactmanager extends FreePBX_Helpers implements BMO {
 						foreach ($type as $value) {
 							switch ($key) {
 								case 'phone':
-									$contact['numbers'][] = array(
+									if (!empty($value['number'])) {
+										$contact['numbers'][] = array(
 											'number' => $value['number'],
 											'type' => isset($value['type']) ? $value['type'] : 'other',
 											'extension' => isset($value['extension']) ? $value['extension'] : '',
 											'flags' => isset($value['flags']) ? explode(',', $value['flags']) : array(),
 											'speeddial' => isset($value['speeddial']) ? $value['speeddial'] : '',
 											'locale' => isset($value['locale']) ? $value['locale'] : '',
-											);
+										);
+									}
 									break;
 								case 'email':
 									$contact['emails'][] = array(
